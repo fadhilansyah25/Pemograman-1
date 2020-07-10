693f67f6b3386e36b785bbc0e7ec799073137f9e
fadhilansyah25 <fadhilansyah25@gmail.com>
2020-07-10 14:24:26 +0700

    delete menu_utama.java & mengubah nama menu_utamacoba menajadi menu utama

--- Database Mahasiswa/src/aplikasi_database_mahasiwa/menu_utama.java
+++ Database Mahasiswa/src/aplikasi_database_mahasiwa/menu_utama.java
@@ -16,6 +16,8 @@ public class menu_utama extends javax.swing.JFrame {
      */
     public menu_utama() {
         initComponents();
+        jLayeredPane1.setVisible(true);
+        jLayeredPane2.setVisible(false);
     }
 
     /**
@@ -27,16 +29,32 @@ public class menu_utama extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jLayeredPane1 = new javax.swing.JLayeredPane();
+        jLabel2 = new javax.swing.JLabel();
         LihatButton = new javax.swing.JButton();
         TambahButton = new javax.swing.JButton();
         UbahButton = new javax.swing.JButton();
         HapusButton = new javax.swing.JButton();
         KeluarButton = new javax.swing.JButton();
         jLabel1 = new javax.swing.JLabel();
-        jLabel2 = new javax.swing.JLabel();
+        jLayeredPane2 = new javax.swing.JLayeredPane();
+        jLabel4 = new javax.swing.JLabel();
+        LihatMatkul_Button = new javax.swing.JButton();
+        TambahMatkul_Button = new javax.swing.JButton();
+        UbahMatkul_Button = new javax.swing.JButton();
+        HapusMatkul_Button = new javax.swing.JButton();
+        KeluarMatkul_Button = new javax.swing.JButton();
+        jLabel5 = new javax.swing.JLabel();
+        jMenuBar1 = new javax.swing.JMenuBar();
+        jMenu1 = new javax.swing.JMenu();
+        jMenu2 = new javax.swing.JMenu();
+        jMenu3 = new javax.swing.JMenu();
+        jMenu4 = new javax.swing.JMenu();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setTitle("Menu Utama");
+
+        jLabel2.setIcon(new javax.swing.ImageIcon("D:\\FADIL\\Belajar CMD-Git-Github\\Pemograman-1\\Database Mahasiswa\\src\\logo_unpam.png")); // NOI18N
 
         LihatButton.setText("Lihat");
         LihatButton.addActionListener(new java.awt.event.ActionListener() {
@@ -76,50 +94,189 @@ public class menu_utama extends javax.swing.JFrame {
         jLabel1.setFont(new java.awt.Font("Tahoma", 1, 14)); // NOI18N
         jLabel1.setText("Menu Utama Database Mahasiswa");
 
-        jLabel2.setIcon(new javax.swing.ImageIcon("D:\\FADIL\\Belajar CMD-Git-Github\\Pemograman-1\\Database Mahasiswa\\src\\logo_unpam.png")); // NOI18N
+        jLayeredPane1.setLayer(jLabel2, javax.swing.JLayeredPane.DEFAULT_LAYER);
+        jLayeredPane1.setLayer(LihatButton, javax.swing.JLayeredPane.DEFAULT_LAYER);
+        jLayeredPane1.setLayer(TambahButton, javax.swing.JLayeredPane.DEFAULT_LAYER);
+        jLayeredPane1.setLayer(UbahButton, javax.swing.JLayeredPane.DEFAULT_LAYER);
+        jLayeredPane1.setLayer(HapusButton, javax.swing.JLayeredPane.DEFAULT_LAYER);
+        jLayeredPane1.setLayer(KeluarButton, javax.swing.JLayeredPane.DEFAULT_LAYER);
+        jLayeredPane1.setLayer(jLabel1, javax.swing.JLayeredPane.DEFAULT_LAYER);
 
-        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
-        getContentPane().setLayout(layout);
-        layout.setHorizontalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addContainerGap(24, Short.MAX_VALUE)
-                .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 299, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(18, 18, 18)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(layout.createSequentialGroup()
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+        javax.swing.GroupLayout jLayeredPane1Layout = new javax.swing.GroupLayout(jLayeredPane1);
+        jLayeredPane1.setLayout(jLayeredPane1Layout);
+        jLayeredPane1Layout.setHorizontalGroup(
+            jLayeredPane1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jLayeredPane1Layout.createSequentialGroup()
+                .addGap(37, 37, 37)
+                .addComponent(jLabel2)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 48, Short.MAX_VALUE)
+                .addGroup(jLayeredPane1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(jLayeredPane1Layout.createSequentialGroup()
+                        .addGroup(jLayeredPane1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
                             .addComponent(UbahButton, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                             .addComponent(LihatButton, javax.swing.GroupLayout.PREFERRED_SIZE, 65, javax.swing.GroupLayout.PREFERRED_SIZE))
                         .addGap(18, 18, 18)
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                        .addGroup(jLayeredPane1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
                             .addComponent(TambahButton)
                             .addComponent(HapusButton, javax.swing.GroupLayout.PREFERRED_SIZE, 71, javax.swing.GroupLayout.PREFERRED_SIZE))
                         .addGap(18, 18, 18)
                         .addComponent(KeluarButton, javax.swing.GroupLayout.PREFERRED_SIZE, 73, javax.swing.GroupLayout.PREFERRED_SIZE))
                     .addComponent(jLabel1))
-                .addGap(54, 54, 54))
+                .addGap(44, 44, 44))
         );
-        layout.setVerticalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addGap(39, 39, 39)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+        jLayeredPane1Layout.setVerticalGroup(
+            jLayeredPane1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jLayeredPane1Layout.createSequentialGroup()
+                .addGap(26, 26, 26)
+                .addGroup(jLayeredPane1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addComponent(jLabel2)
-                    .addGroup(layout.createSequentialGroup()
+                    .addGroup(jLayeredPane1Layout.createSequentialGroup()
+                        .addGap(40, 40, 40)
                         .addComponent(jLabel1)
-                        .addGap(57, 57, 57)
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                        .addGap(34, 34, 34)
+                        .addGroup(jLayeredPane1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                             .addComponent(KeluarButton, javax.swing.GroupLayout.PREFERRED_SIZE, 60, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addGroup(layout.createSequentialGroup()
-                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addGroup(jLayeredPane1Layout.createSequentialGroup()
+                                .addGroup(jLayeredPane1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                                     .addComponent(LihatButton)
                                     .addComponent(TambahButton))
                                 .addGap(18, 18, 18)
-                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                                .addGroup(jLayeredPane1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                                     .addComponent(UbahButton)
                                     .addComponent(HapusButton))))))
-                .addGap(0, 47, Short.MAX_VALUE))
+                .addContainerGap(33, Short.MAX_VALUE))
+        );
+
+        jLabel4.setIcon(new javax.swing.ImageIcon("D:\\FADIL\\Belajar CMD-Git-Github\\Pemograman-1\\Database Mahasiswa\\src\\logo_unpam.png")); // NOI18N
+
+        LihatMatkul_Button.setText("Lihat");
+        LihatMatkul_Button.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                LihatMatkul_ButtonActionPerformed(evt);
+            }
+        });
+
+        TambahMatkul_Button.setText("Tambah");
+        TambahMatkul_Button.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                TambahMatkul_ButtonActionPerformed(evt);
+            }
+        });
+
+        UbahMatkul_Button.setText("Ubah");
+        UbahMatkul_Button.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                UbahMatkul_ButtonActionPerformed(evt);
+            }
+        });
+
+        HapusMatkul_Button.setText("Hapus");
+        HapusMatkul_Button.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                HapusMatkul_ButtonActionPerformed(evt);
+            }
+        });
+
+        KeluarMatkul_Button.setText("Keluar");
+        KeluarMatkul_Button.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                KeluarMatkul_ButtonActionPerformed(evt);
+            }
+        });
+
+        jLabel5.setFont(new java.awt.Font("Tahoma", 1, 14)); // NOI18N
+        jLabel5.setText("Menu Utama Database Matakuliah");
+
+        jLayeredPane2.setLayer(jLabel4, javax.swing.JLayeredPane.DEFAULT_LAYER);
+        jLayeredPane2.setLayer(LihatMatkul_Button, javax.swing.JLayeredPane.DEFAULT_LAYER);
+        jLayeredPane2.setLayer(TambahMatkul_Button, javax.swing.JLayeredPane.DEFAULT_LAYER);
+        jLayeredPane2.setLayer(UbahMatkul_Button, javax.swing.JLayeredPane.DEFAULT_LAYER);
+        jLayeredPane2.setLayer(HapusMatkul_Button, javax.swing.JLayeredPane.DEFAULT_LAYER);
+        jLayeredPane2.setLayer(KeluarMatkul_Button, javax.swing.JLayeredPane.DEFAULT_LAYER);
+        jLayeredPane2.setLayer(jLabel5, javax.swing.JLayeredPane.DEFAULT_LAYER);
+
+        javax.swing.GroupLayout jLayeredPane2Layout = new javax.swing.GroupLayout(jLayeredPane2);
+        jLayeredPane2.setLayout(jLayeredPane2Layout);
+        jLayeredPane2Layout.setHorizontalGroup(
+            jLayeredPane2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jLayeredPane2Layout.createSequentialGroup()
+                .addGap(37, 37, 37)
+                .addComponent(jLabel4)
+                .addGap(48, 48, 48)
+                .addGroup(jLayeredPane2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(jLayeredPane2Layout.createSequentialGroup()
+                        .addGroup(jLayeredPane2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                            .addComponent(UbahMatkul_Button, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                            .addComponent(LihatMatkul_Button, javax.swing.GroupLayout.PREFERRED_SIZE, 65, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addGap(18, 18, 18)
+                        .addGroup(jLayeredPane2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                            .addComponent(TambahMatkul_Button)
+                            .addComponent(HapusMatkul_Button, javax.swing.GroupLayout.PREFERRED_SIZE, 71, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addGap(18, 18, 18)
+                        .addComponent(KeluarMatkul_Button, javax.swing.GroupLayout.PREFERRED_SIZE, 73, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(jLabel5))
+                .addContainerGap(44, Short.MAX_VALUE))
+        );
+        jLayeredPane2Layout.setVerticalGroup(
+            jLayeredPane2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jLayeredPane2Layout.createSequentialGroup()
+                .addGap(26, 26, 26)
+                .addGroup(jLayeredPane2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jLabel4)
+                    .addGroup(jLayeredPane2Layout.createSequentialGroup()
+                        .addGap(40, 40, 40)
+                        .addComponent(jLabel5)
+                        .addGap(34, 34, 34)
+                        .addGroup(jLayeredPane2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(KeluarMatkul_Button, javax.swing.GroupLayout.PREFERRED_SIZE, 60, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addGroup(jLayeredPane2Layout.createSequentialGroup()
+                                .addGroup(jLayeredPane2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                                    .addComponent(LihatMatkul_Button)
+                                    .addComponent(TambahMatkul_Button))
+                                .addGap(18, 18, 18)
+                                .addGroup(jLayeredPane2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                                    .addComponent(UbahMatkul_Button)
+                                    .addComponent(HapusMatkul_Button))))))
+                .addContainerGap(33, Short.MAX_VALUE))
+        );
+
+        jMenu1.setText("File");
+        jMenuBar1.add(jMenu1);
+
+        jMenu2.setText("Edit");
+        jMenuBar1.add(jMenu2);
+
+        jMenu3.setText("Data Mahasiswa");
+        jMenu3.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                jMenu3MouseClicked(evt);
+            }
+        });
+        jMenuBar1.add(jMenu3);
+
+        jMenu4.setText("Data Matakuliah");
+        jMenu4.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                jMenu4MouseClicked(evt);
+            }
+        });
+        jMenuBar1.add(jMenu4);
+
+        setJMenuBar(jMenuBar1);
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(jLayeredPane1)
+            .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                .addComponent(jLayeredPane2, javax.swing.GroupLayout.Alignment.TRAILING))
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(jLayeredPane1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+            .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                .addComponent(jLayeredPane2))
         );
 
         pack();
@@ -150,6 +307,45 @@ public class menu_utama extends javax.swing.JFrame {
         new Hapus_dataMahasiswa().setVisible(true);
     }//GEN-LAST:event_HapusButtonActionPerformed
 
+    private void jMenu3MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jMenu3MouseClicked
+        // TODO add your handling code here:
+        jLayeredPane1.setVisible(true);
+        jLayeredPane2.setVisible(false);
+        setTitle("Menu Utama - Database Mahasiswa");
+    }//GEN-LAST:event_jMenu3MouseClicked
+
+    private void jMenu4MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jMenu4MouseClicked
+        // TODO add your handling code here:
+        jLayeredPane1.setVisible(false);
+        jLayeredPane2.setVisible(true);
+        setTitle("Menu Utama - Database Mata Kuliah");
+    }//GEN-LAST:event_jMenu4MouseClicked
+
+    private void LihatMatkul_ButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_LihatMatkul_ButtonActionPerformed
+        // TODO add your handling code here:
+        new tabel_dataMatakuliah().setVisible(true);
+    }//GEN-LAST:event_LihatMatkul_ButtonActionPerformed
+
+    private void TambahMatkul_ButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_TambahMatkul_ButtonActionPerformed
+        // TODO add your handling code here:
+        new Input_dataMatakuliah().setVisible(true);
+    }//GEN-LAST:event_TambahMatkul_ButtonActionPerformed
+
+    private void UbahMatkul_ButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_UbahMatkul_ButtonActionPerformed
+        // TODO add your handling code here:
+        new Ubah_dataMatakuliah().setVisible(true);
+    }//GEN-LAST:event_UbahMatkul_ButtonActionPerformed
+
+    private void HapusMatkul_ButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_HapusMatkul_ButtonActionPerformed
+        // TODO add your handling code here:
+        new Hapus_dataMatakuliah().setVisible(true);
+    }//GEN-LAST:event_HapusMatkul_ButtonActionPerformed
+
+    private void KeluarMatkul_ButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_KeluarMatkul_ButtonActionPerformed
+        // TODO add your handling code here:
+        System.exit(0);
+    }//GEN-LAST:event_KeluarMatkul_ButtonActionPerformed
+
     /**
      * @param args the command line arguments
      */
@@ -176,6 +372,9 @@ public class menu_utama extends javax.swing.JFrame {
             java.util.logging.Logger.getLogger(menu_utama.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
@@ -187,11 +386,25 @@ public class menu_utama extends javax.swing.JFrame {
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton HapusButton;
+    private javax.swing.JButton HapusMatkul_Button;
     private javax.swing.JButton KeluarButton;
+    private javax.swing.JButton KeluarMatkul_Button;
     private javax.swing.JButton LihatButton;
+    private javax.swing.JButton LihatMatkul_Button;
     private javax.swing.JButton TambahButton;
+    private javax.swing.JButton TambahMatkul_Button;
     private javax.swing.JButton UbahButton;
+    private javax.swing.JButton UbahMatkul_Button;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel4;
+    private javax.swing.JLabel jLabel5;
+    private javax.swing.JLayeredPane jLayeredPane1;
+    private javax.swing.JLayeredPane jLayeredPane2;
+    private javax.swing.JMenu jMenu1;
+    private javax.swing.JMenu jMenu2;
+    private javax.swing.JMenu jMenu3;
+    private javax.swing.JMenu jMenu4;
+    private javax.swing.JMenuBar jMenuBar1;
     // End of variables declaration//GEN-END:variables
 }
